d41d8930d5113e00dad75960d5eb0e69ac388143
Milkey Tan <819774989@qq.com>
2018-11-05 10:58:51 +0800

    no message

---
+++ SubdomainScanner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubdomainScanner
+{
+    class SubdomainScanner
+    {
+        public static Dictionary<string,IPAddress[]> Scan(string domain,IEnumerable<string> dict)
+        {
+            Dictionary<string, IPAddress[]> result = new Dictionary<string, IPAddress[]>();
+
+            foreach (var item in dict)
+            {
+
+                string subName = $"{item}.{domain}";
+                try
+                {
+                    var dnsIPs = Dns.GetHostAddresses(subName);
+                    result.Add(item,dnsIPs);
+                    Console.WriteLine($"{subName} : {string.Join(", ", Array.ConvertAll(dnsIPs, s => s.ToString()))}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{subName} : {e.Message}");
+                }
+            }
+
+            return result;
+        }
+    }
+}
